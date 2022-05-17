13e3516530e1e03de05debc9a253c640a8913c0f
4th-August <942267027@qq.com>
2022-05-17 14:29:13 +0800

    This is my Second day committing code...@2022年5月17日 13:53:55 周 二

---
+++ container_api.php
@@ -0,0 +1,29 @@
+<?php
+
+$resources = array(
+    'Article',
+    'ArticleCategories',
+    'Articles',
+    'Classroom',
+    'ClassroomMember',
+    'ClassroomMembers',
+    'Classrooms',
+    'CourseMember',
+    'CourseMembers',
+    'Exercise',
+    'ExerciseResult',
+    'MeChatroomes',
+    'User',
+    'Apps',
+    'App',
+    'Homework',
+    'HomeworkResult',
+    'Upload'
+);
+
+foreach ($resources as $res) {
+    $app["res.{$res}"] = $app->share(function() use ($res) {
+        $class = "Topxia\\Api\\Resource\\{$res}";
+        return new $class();
+    });
+}
